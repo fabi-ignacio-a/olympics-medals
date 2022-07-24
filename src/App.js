95c1c3efecc94e1a78307f867e942f5c5a7e7354
Fabián Alarcón <ext_fandgalazs@falabella.cl>
2022-07-24 10:21:14 -0400

    Recuperada la data de la bd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 
+  React,
+  { 
+    useState,
+    useEffect  
+  }
+from 'react';
+
+const URL = 'http://localhost:4000/countries';
+
 function App() {
+
+  const [countries, setCountries] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(
+    () => {
+        const fetchCountries = async() => {
+        const response = await fetch(URL);
+        const countries = await response.json();
+        console.log(countries);
+        setCountries(countries);
+        setIsLoading(false);
+      }
+      fetchCountries();
+    }, [])
+
+  if(isLoading){
+    return <p>... Loading</p>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> Cuadro de medallas </h3>
+        <div>
+        {
+            countries.map(country => (
+              <p key = {country.id}> {country.name} </p>
+            ))
+          }
+        </div>
       </header>
     </div>
   );
